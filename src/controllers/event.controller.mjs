b87e1ccb8a01ce477ed7b302611688792a157adb
IIshimwe <ixacson@gmail.com>
2024-04-30 23:28:56 +0200

    Add get single event, update event, and delete event route. Also prevent double event to be created

--- src/controllers/event.controller.mjs
+++ src/controllers/event.controller.mjs
@@ -1,3 +1,5 @@
+import moment from "moment";
+import _ from "lodash";
 import Event from "../../db/models/event.model.mjs";
 import { validateEvent } from "../helpers/validations.mjs";
 
@@ -10,6 +12,32 @@ export const createEvent = async (req, res) => {
   const { error } = validateEvent(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const event = await Event.create(req.body);
+  let event = await Event.findOne({ title: req.body.title });
+  if (event)
+    return res.status(400).send("Event with the given title already exists");
+
+  event = await Event.create(req.body);
   res.status(200).json({ event, message: "Create event" });
+};
+
+export const getSingleEvent = async (req, res) => {
+  const event = await Event.findById(req.params.id);
+  if (!event) return res.status(404).send("No such event found.");
+
+  res.status(200).send(event);
+};
+
+export const updateEvent = async (req, res) => {
+  const { error } = validateEvent(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json({ event, message: "Update event" });
+};
+
+export const deleteEvent = async (req, res) => {
+  const event = await Event.findByIdAndDelete(req.params.id);
+  res.status(200).json({ event, message: "Delete event" });
 };
--- src/routes/event.route.mjs
+++ src/routes/event.route.mjs
@@ -1,7 +1,17 @@
 import express from "express";
 const router = express.Router();
-import { createEvent, getAllEvents } from "../controllers/event.controller.mjs";
+import {
+  createEvent,
+  deleteEvent,
+  getAllEvents,
+  getSingleEvent,
+  updateEvent,
+} from "../controllers/event.controller.mjs";
 
 router.get("/", getAllEvents);
 router.post("/", createEvent);
+router.get("/:id", getSingleEvent);
+router.put("/:id", updateEvent);
+router.delete("/:id", deleteEvent);
+
 export default router;
